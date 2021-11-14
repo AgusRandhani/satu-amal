c914ec61f61d5818959706ede4a0aaf3a52c500c
agusrr93 <agus.randhani@versafleet.co>
2021-11-14 19:55:50 +0700

    fix

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,7 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _application = require('./application'); var _application2 = _interopRequireDefault(_application);
 
-_application2.default.listen(process.env.APP_PORT, () => {
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+var server_port = process.env.APP_PORT || process.env.PORT || 3333;
+_application2.default.listen(server_port, server_host, () => {
   console.log(`Server is running on :${process.env.APP_URL}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import app from "./application";
 
-app.listen(process.env.APP_PORT, () => {
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+var server_port = process.env.APP_PORT || process.env.PORT || 3333;
+app.listen(server_port, server_host, () => {
   console.log(`Server is running on :${process.env.APP_URL}`);
 });
