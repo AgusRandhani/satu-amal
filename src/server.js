474b9fab85c025575c0fbeb188ca97dcf1d8802d
agusrr93 <agus.randhani@versafleet.co>
2021-11-14 18:41:42 +0700

    deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from './application'
+import app from "./application";
 
 app.listen(process.env.APP_PORT, () => {
-  console.log(`Server is running on :${process.env.APP_URL}`)
-})
+  console.log(`Server is running on :${process.env.APP_URL}`);
+});
